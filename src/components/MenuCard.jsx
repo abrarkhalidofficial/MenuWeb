8ce4a319abfce18113e8362f24d1f55d57f2b6c1
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-01-19 15:06:50 +0500

    Update MenuCard.jsx

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
 import item from "../assets/itemimg.png";
 import star from "../assets/star.png";
-export default function MenuCard() {
+import { useState } from "react";
+
+export default function MenuCard({ product }) {
   const [buttonText, setButtonText] = useState("Add to Cart");
   const handleButtonClick = () => {
     if (buttonText === "Add to Cart") {
@@ -13,13 +14,13 @@ export default function MenuCard() {
   return (
     <div className="menu__home__content__right__content__bottom__content__items__card">
       <div className="menu__home__content__right__content__bottom__content__items__card__foodimg">
-        <img src={item} alt="item" />
+        <img src={product.strMealThumb} alt="item" />
       </div>
       <div className="menu__home__content__right__content__bottom__content__items__card__ratingimg">
         <img src={star} alt="star" />
       </div>
       <div className="menu__home__content__right__content__bottom__content__items__card__name">
-        Malang Jaan Beef
+        {product.strMeal}
       </div>
       <div className="menu__home__content__right__content__bottom__content__items__card__weight">
         500g
