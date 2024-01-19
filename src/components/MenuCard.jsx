afea7faec9f4a890ab2927b9c9d36477de11f338
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-01-19 16:33:34 +0500

    Update MenuCard.jsx

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,42 +1,57 @@
-import item from "../assets/itemimg.png";
+import { cartAtom } from "../data/cartAtom";
 import star from "../assets/star.png";
-import { useState } from "react";
+import { useAtom } from "jotai";
+import { useMemo } from "react";
 
 export default function MenuCard({ product }) {
-  const [buttonText, setButtonText] = useState("Add to Cart");
-  const handleButtonClick = () => {
-    if (buttonText === "Add to Cart") {
-      setButtonText("Added to Cart");
+  const [cart, setCart] = useAtom(cartAtom);
+  const isInCart = cart.includes(product?.strMeal);
+
+  const handleAddToCart = () => {
+    if (isInCart) {
+      const newCart = cart.filter((item) => item !== product?.strMeal);
+      setCart(newCart);
     } else {
-      setButtonText("Add to Cart");
+      setCart([...cart, product?.strMeal]);
     }
   };
+
+  const randomWeight = useMemo(() => Math.floor(Math.random() * 500), []);
+
+  const randomPrice = useMemo(() => Math.floor(Math.random() * 1000), []);
+
   return (
-    <div className="menu__home__content__right__content__bottom__content__items__card">
+    <div
+      className={
+        product
+          ? "menu__home__content__right__content__bottom__content__items__card"
+          : "menu__home__content__right__content__bottom__content__items__card menu__home__content__right__content__bottom__content__items__card__empty"
+      }
+    >
       <div className="menu__home__content__right__content__bottom__content__items__card__foodimg">
-        <img src={product.strMealThumb} alt="item" />
+        <img src={product?.strMealThumb} alt="item" />
       </div>
       <div className="menu__home__content__right__content__bottom__content__items__card__ratingimg">
         <img src={star} alt="star" />
       </div>
       <div className="menu__home__content__right__content__bottom__content__items__card__name">
-        {product.strMeal}
+        {product?.strMeal}
       </div>
       <div className="menu__home__content__right__content__bottom__content__items__card__weight">
-        500g
+        {randomWeight}gm
       </div>
       <div className="menu__home__content__right__content__bottom__content__items__card__price">
-        AED 600
+        AED {randomPrice}
       </div>
       <button
-        onClick={handleButtonClick}
+        onClick={handleAddToCart}
         className={
-          buttonText === "Add to Cart"
-            ? "menu__home__content__right__content__bottom__content__items__card__button"
-            : "menu__home__content__right__content__bottom__content__items__card__button__active"
+          isInCart
+            ? "menu__home__content__right__content__bottom__content__items__card__button menu__home__content__right__content__bottom__content__items__card__button__active"
+            : "menu__home__content__right__content__bottom__content__items__card__button"
         }
       >
-        {buttonText}
+        {isInCart ? "Added" : "Add to Cart"}
       </button>
     </div>
   );
