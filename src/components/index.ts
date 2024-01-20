4a9d3eb58ec147847420dbbbe40cdbd833f0f368
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-01-20 18:00:37 +0500

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import CartPopup from './CartPopup.jsx'
 import Category from './Category.jsx'
+import LanguageButton from './LanguageButton.jsx'
 import MenuCard from './MenuCard.jsx'
 
 export {
   CartPopup,
   Category,
+  LanguageButton,
   MenuCard
 }
