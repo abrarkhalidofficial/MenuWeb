13660069ba199bf2db2f0d464153aa27b2ce1249
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-01-20 18:47:26 +0500

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,7 @@
 import PngItemimg from './itemimg.png'
 import SvgLogo from './logo.svg'
+import SvgLogoar from './logoar.svg'
+import SvgLogoarwhite from './logoarwhite.svg'
 import SvgLogodark from './logodark.svg'
 import PngStar from './star.png'
 import PngStardark from './stardark.png'
@@ -7,6 +9,8 @@ import PngStardark from './stardark.png'
 export {
   PngItemimg,
   SvgLogo,
+  SvgLogoar,
+  SvgLogoarwhite,
   SvgLogodark,
   PngStar,
   PngStardark
