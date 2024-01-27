a8103466261b708b4b8f6d07dcfa9e1cb96a9e03
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-01-27 21:37:26 +0500

    Update App.jsx

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,11 +1,9 @@
-import LanguageButton from "../components/LanguageButton";
 import LanguageProvider from "../context/LanguageContext";
 import { Outlet } from "react-router-dom";
 
 export default () => {
   return (
     <LanguageProvider>
-      <LanguageButton />
       <Outlet />
     </LanguageProvider>
   );
