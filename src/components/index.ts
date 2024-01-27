35e02bdc0e3298e717969febcf9346225724ec5e
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-01-27 21:36:25 +0500

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,21 @@
-import CartPopup from './CartPopup.jsx'
 import Category from './Category.jsx'
+import Header from './Header.jsx'
 import LanguageButton from './LanguageButton.jsx'
 import Magnetic from './Magnetic.jsx'
 import MenuCard from './MenuCard.jsx'
+import MobileCategories from './MobileCategories.jsx'
+import ProductPopup from './ProductPopup.jsx'
 import RoundedButton from './RoundedButton.jsx'
+import Sidebar from './Sidebar.jsx'
 
 export {
-  CartPopup,
   Category,
+  Header,
   LanguageButton,
   Magnetic,
   MenuCard,
-  RoundedButton
+  MobileCategories,
+  ProductPopup,
+  RoundedButton,
+  Sidebar
 }
