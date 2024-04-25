50bf86d1b366f3f29a97abb7e69bd84682563180
Arslan Iftikhar <a44222666@gmail.com>
2024-04-25 13:18:17 +0500

    Update ProductPopup.jsx

--- src/components/ProductPopup.jsx
+++ src/components/ProductPopup.jsx
@@ -66,6 +66,32 @@ function ProductPopup() {
                 : "is__closed")
             }
           >
+            <button
+              onClick={() => {
+                setDataForProductPopup({});
+                setAddedAdditives([]);
+                setAddedVariants([]);
+              }}
+              class="imagespopup__img__closebutton"
+              style={{
+                zIndex: 1000,
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
+            </button>
             <div className="menu__home__content__popup__content__slider">
               <img
                 loading="lazy"
