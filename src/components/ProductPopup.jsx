5a6ac27954ff56bb57d8d5491acddd42e53dddd9
Arslan Iftikhar <a44222666@gmail.com>
2024-04-25 15:26:15 +0500

    Update ProductPopup.jsx

--- src/components/ProductPopup.jsx
+++ src/components/ProductPopup.jsx
@@ -181,33 +181,24 @@ function ProductPopup() {
                       textAlign: selectedLanguage === "ar" ? "right" : "left",
                     }}
                   >
-                    <div
-                      className="menu__home__content__popup__content__slider__info__allergy__heading"
-                      style={{
-                        marginBottom: 5,
-                        marginTop: 5,
-                        fontWeight: 600,
-                        fontSize: 16,
-                      }}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      class="feather feather-zap-off"
                     >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18"
-                        height="18"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        class="feather feather-zap-off"
-                      >
-                        <polyline points="12.41 6.75 13 2 10.57 4.92"></polyline>
-                        <polyline points="18.57 12.91 21 10 15.66 10"></polyline>
-                        <polyline points="8 8 3 14 12 14 11 22 16 16"></polyline>
-                        <line x1="1" y1="1" x2="23" y2="23"></line>
-                      </svg>
-                    </div>
+                      <polyline points="12.41 6.75 13 2 10.57 4.92"></polyline>
+                      <polyline points="18.57 12.91 21 10 15.66 10"></polyline>
+                      <polyline points="8 8 3 14 12 14 11 22 16 16"></polyline>
+                      <line x1="1" y1="1" x2="23" y2="23"></line>
+                    </svg>
+
                     {dataForProductPopup?.allergies?.map((allergy, index) =>
                       index === dataForProductPopup?.allergies?.length - 1 ? (
                         <span key={allergy}>
